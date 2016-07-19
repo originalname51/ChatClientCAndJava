d168dedbfe08a695d17827787175073c6ead57d9
rob <robwstine@outlook.com>
2016-07-19 19:40:34 -0400

    first commit

---
+++ src/network_one/MainClass.java
@@ -0,0 +1,18 @@
+package network_one;
+
+
+public class MainClass {
+	
+	/*
+	 * Class Project
+	 * 
+	 * */
+
+	public static void main(String[] args) {
+
+
+		System.out.println("hello world");
+	}
+
+}
+
