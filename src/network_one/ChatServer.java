b99f7865c3bbdf662194d79dc090e9a62678a2ef
rob <robwstine@outlook.com>
2016-07-23 21:02:58 -0400

    Patched everything up. This will net full credit.

--- src/network_one/ChatServer.java
+++ src/network_one/ChatServer.java
@@ -13,53 +13,70 @@ private         Socket client;
 private    PrintWriter out;
 private BufferedReader in;
 private BufferedReader consoleMessage;
-private final int    MESSAGE_LENGTH = 500;
-private final int	 CHAR_OFFSET    = 100;
+private final int  	   MESSAGE_LENGTH = 500;
+private final int	   CHAR_OFFSET    = 100;
 
+/*
+ * Chat Server Start Up - takes and starts the chat server.
+ * Creates a reader for console messages.
+ * */
 public	ChatServer(int portnumber)
 		{
 		try {
 			this.chatServer = new ServerSocket(portnumber);
+			this.consoleMessage = new BufferedReader(new InputStreamReader(System.in));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Critical Error. Socket Failed");
+			System.out.println("Critical Error. Socket Failed.");
 			e.printStackTrace();
 		}		
 	}
-
+/*
+ * This will accept a connection. 
+ * It will accept a connection from a client
+ * It will create an output (using a print writer)
+ * It will also create an input.
+ * http://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+ * */
 public void acceptConnection()
 {		
 	try {
 		this.client 		= this.chatServer.accept();
 		this.out			= new PrintWriter(this.client.getOutputStream(),true);
 		this.in				= new BufferedReader(new InputStreamReader(this.client.getInputStream()));
-		this.consoleMessage = new BufferedReader(new InputStreamReader(System.in));
 	} catch (IOException e) {
-		// TODO Auto-generated catch block
+		shutDown(); //if it fails print a function specific message and shut down the connection.
 		System.out.println("Critical Error. Accept Connection Failed");
 		e.printStackTrace();
 	}
 
 }
 //http://stackoverflow.com/questions/4644415/java-how-to-get-input-from-system-console
+//http://stackoverflow.com/questions/2340106/what-is-the-purpose-of-flush-in-java-streams
 public boolean sendMessage()
 {
 	String sendme;
 	try {
 		sendme = consoleMessage.readLine();
-		out.println(Integer.toString(sendme.length() +CHAR_OFFSET+1));
+		out.println(Integer.toString(sendme.length() + CHAR_OFFSET+1));
 		out.flush();
 		out.println(sendme);
 		return _checkQuit(sendme);
 	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		
+		shutDown();
 		e.printStackTrace();
 	}
 	
 	return false;
 }
-
+/*
+ * //http://stackoverflow.com/questions/8694984/remove-part-of-string
+ * 
+ * This will recieve a message. It first takes a newline character value from the client.
+ * It will parse this as an integer value. It will then 
+ * 
+ * Protocol is client sends how long a message is, and then the message itself.
+ * http://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html
+ * */
 public boolean recieveMessage()
 {
 	String howLongMessageIs;
@@ -67,44 +84,63 @@ public boolean recieveMessage()
 	char [] message = new char[MESSAGE_LENGTH];
 	int CharsToRead = 0;
 
+	// First read how large the message is. Note it will always be the CHAR_OFFSET more in order to have a constant
+	// integer size value (i.e. 105 instead of '5', 125 instead of 25).
+	// This allows for values up to 900 characters long, 400 more than the specifications require.
 	try {
 	      howLongMessageIs = in.readLine();
 	      CharsToRead      = Integer.parseInt(howLongMessageIs.trim());
 	      CharsToRead     -= CHAR_OFFSET;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			shutDown();
 			System.out.println("FAILED IN RECIEVE MESSAGE");
 			e.printStackTrace();
-		}			
+		}	
+	
+	/*
+	 * This will read in the full message from the client. 
+	 * It will then display it, add a new line and check to see if the client wants to \quit.
+	 * */
 	try {
-		if(in.read(message,0,CharsToRead-1) == -1)
-			System.out.println("Critical Error. File not read correctly.");
+		if(in.read(message,0,CharsToRead) == -1)
+		System.out.println("Critical Error. File not read correctly.");
 		outputMessage = String.valueOf(message);
-		System.out.println(outputMessage.trim());
-		
-		return _checkQuit(outputMessage);
+		System.out.print(outputMessage.trim());	//this will print instead of println to preserve all sent newlines.
+		System.out.print("\n");					//This will print a newline to seperate server send values.
+
+		//The +2 here is because ">" = 1 and there is always a space after it, (2).
+		int index = outputMessage.indexOf(">");
+		String checker = outputMessage.substring(index+2, outputMessage.length());
+		return _checkQuit(checker);
 		
 	} catch (IOException e) {
 		System.out.println("FAILED IN GETTING MESSAGE FROM SENDER! CRITICAL ERROR");
-		// TODO Auto-generated catch block
+		shutDown();
 		e.printStackTrace();
 	}
 	return false;
 }
-
+/*
+ * Helper function. Checks to see if string is "\quit".
+ * */
 private boolean _checkQuit(String check)
 {
 	return (check.trim().equals("\\quit")) ? false : true;	
 }
+
+/*
+ * This function will shut down the server.
+ * */
 public void shutDown()
 {
 	try {
 		this.in.close();
-		this.out.close();
 		this.client.close();
 	} catch (IOException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
+	}
+	finally{
+		this.out.close();
 	}
 }
 	
--- src/network_one/MainClass.java
+++ src/network_one/MainClass.java
@@ -3,21 +3,23 @@ package network_one;
 public class MainClass {
 	/*
 	 * Class Project
-	 * 
+	 * This will run in an infinite loop.
+	 * It will create a chatserver and then accept connections allowing for back and forth client/server communication.
 	 */
 	public static void main(String[] args) {
 		ChatServer chatserver = new ChatServer(Integer.parseInt(args[0]));
 		while (true) {
+			System.out.println("ChatServer awaiting connection on port " + args[0]);
 			chatserver.acceptConnection();
 			while (true) {	
-				if (!(chatserver.sendMessage())) {
-					chatserver.shutDown();
-					break;
-				}	
 				if (!(chatserver.recieveMessage())) {
 					chatserver.shutDown();
 					break;		
 				}
+				if (!(chatserver.sendMessage())) {
+					chatserver.shutDown();
+					break;
+				}	
 			}
 		}
 	}
